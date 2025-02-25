7c9445b69ed12eebe6a22767e944459242a8b339
Nikola Kozhuharov <nikola.kozhuharov@waterdrop.com>
2025-02-25 20:04:18 +0100

    Some updates and fixes

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,24 @@
 import './App.css'
 import CalculatorModal from './components/CalculatorModal'
 import {useState} from "react";
+import {faCalculator} from '@fortawesome/free-solid-svg-icons'
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 
 function App() {
-
-    let [modalIsShown, setModalIsShown] = useState(false);
-
-    const handleOpenModal = () => {
-        setModalIsShown(true);
-    };
+    let [modalIsShown, setModalVisible] = useState(false);
+    let [modalValue, setModalValue] = useState('');
 
     return (
         <>
-            <CalculatorModal showModal={modalIsShown}/>
+            <CalculatorModal isShown={modalIsShown} initialValue={modalValue} setModalValue={setModalValue} setModalVisible={setModalVisible}/>
+            <h1>Calculator</h1>
+            <div className="input-group">
+                <input type="text" className='form-control' placeholder='0.00' value={modalValue} onChange={(e) => setModalValue(e.target.value)}/>
 
-            <button type="button" onClick={handleOpenModal}>
-                Show Modal
-            </button>
+                <button type="button" className="btn btn-primary" onClick={() => setModalVisible(true)}>
+                    <FontAwesomeIcon icon={faCalculator}/>
+                </button>
+            </div>
         </>
     )
 }
--- src/components/CalculatorModal.jsx
+++ src/components/CalculatorModal.jsx
@@ -1,14 +1,17 @@
-import {useState} from "react";
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
 import {faArrowLeft, faArrowRight, faDivide, faEquals, faMinus, faMultiply, faPlus, faX} from '@fortawesome/free-solid-svg-icons'
 import {evaluate} from "mathjs";
+import {useEffect, useState} from "react";
 
-export default function CalculatorModal(props) {
-    let [modalIsShown, setModalIsShown] = useState(props.showModal || true);
-
+export default function CalculatorModal({isShown, initialValue, setModalValue, setModalVisible}) {
     const mathSymbols = ['÷', 'x', '+', '-'];
 
-    let [calculatorInputValue, setCalculatorInputValue] = useState('');
+    let [calculatorInputValue, setCalculatorInputValue] = useState(initialValue ? initialValue.toString() : '');
+
+    useEffect(() => {
+        // Update the internal state whenever initialValue changes
+        setCalculatorInputValue(initialValue);
+    }, [initialValue]);
 
     function safeEval(expression) {
         if (expression === '') {
@@ -25,17 +28,19 @@ export default function CalculatorModal(props) {
     }
 
     const handleCloseCalculator = () => {
-        setModalIsShown(false);
+        setModalVisible(false);
     };
 
     const handleInputButtonClick = (event) => {
         const value = event.currentTarget.getAttribute('data-value');
 
-        if (calculatorInputValue.includes('.') && value === '.') {
-            return;
-        }
+        setCalculatorInputValue(function (currentValue) {
+            if (currentValue.includes('.') && value === '.') {
+                return currentValue;
+            }
 
-        setCalculatorInputValue(calculatorInputValue + value);
+            return currentValue + value;
+        });
     };
 
     const handleClearButtonClick = () => {
@@ -43,31 +48,41 @@ export default function CalculatorModal(props) {
     }
 
     const handleBackspaceButtonClick = () => {
-        setCalculatorInputValue(calculatorInputValue.length > 0 ? calculatorInputValue.substring(0, calculatorInputValue.length - 1) : '');
+        setCalculatorInputValue(function (currentValue) {
+            return currentValue.length > 0 ? currentValue.substring(0, currentValue.length - 1) : '';
+        });
     }
 
     const handleMathSymbolButtonClick = (event) => {
         const value = event.currentTarget.getAttribute('data-value');
-        const lastChar = calculatorInputValue.slice(-1);
 
-        if (calculatorInputValue.length === 0 || mathSymbols.includes(lastChar)) {
-            return;
-        }
+        setCalculatorInputValue(function (currentValue) {
+            const lastChar = currentValue.slice(-1);
 
-        setCalculatorInputValue(calculatorInputValue + value);
+            if (currentValue.length === 0 || mathSymbols.includes(lastChar)) {
+                return currentValue;
+            }
+            return currentValue + value
+        });
     }
 
     const handleEqualsButtonClick = () => {
-        const result = safeEval(calculatorInputValue);
-        setCalculatorInputValue(result);
+        setCalculatorInputValue(function (currentValue) {
+            return safeEval(currentValue);
+        });
     }
 
-    return (
-        <div className='modal' style={{display: modalIsShown ? 'block' : 'none'}} id="calculator-modal">
-            <div className="modal-dialog calculator-modal">
+    const handleConfirmButtonClick = () => {
+        setModalValue(safeEval(calculatorInputValue));
+        setModalVisible(false);
+    }
+
+    return isShown ? (
+        <div className='modal' style={{display: 'block'}}>
+            <div className="modal-dialog">
                 <div className="modal-content">
                     <div className="modal-header">
-                        <h4 className="modal-title">Enter Amount</h4>
+                        <h5 className="modal-title">Enter Amount</h5>
                         <button type="button" id="close-calculator" className="btn btn-danger fa-pull-right" onClick={handleCloseCalculator}>
                             <FontAwesomeIcon icon={faX}/>
                         </button>
@@ -145,12 +160,12 @@ export default function CalculatorModal(props) {
                                 <button type="button" className="btn btn-light w-100" onClick={handleInputButtonClick} data-value=".">.</button>
                             </div>
                             <div className="col-3">
-                                <button type="button" className="btn btn-success w-100 confirm-button"><FontAwesomeIcon icon={faArrowRight}/></button>
+                                <button type="button" className="btn btn-success w-100 confirm-button" onClick={handleConfirmButtonClick}><FontAwesomeIcon icon={faArrowRight}/></button>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-    );
+    ) : null;
 }
